91310256c28389c24384efe3e1fd45ccc627899a
srishti1123 <gsrishti111@gmail.com>
2024-10-23 23:20:38 +0530

    add script

---
+++ scripts/mvn_instrument_run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Set Datadog-related environment variables
+export DD_SERVICE="Datadog-java-0.0.1-SNAPSHOT"
+export DD_VERSION="1.0.0"
+export DD_ENV="staging"
+
+
+# Java options
+JAVA_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED"
+
+# Build the project
+./mvnw clean package
+
+# Check if the JAR file exists before running it
+if [ -f target/Datadog-java-0.0.1-SNAPSHOT.jar ]; then
+    echo "Using agent path: C:\Users\Srishti\Downloads\Datadog-java\Datadog-java\dd-java-agent.jar"
+    echo "Using JAR path: target/Datadog-java-0.0.1-SNAPSHOT.jar"
+
+    # Run the application with Datadog Java agent
+    java $JAVA_OPTS -javaagent:C:/Users/Srishti/Downloads/Datadog-java/dd-java-agent.jar \
+        -Ddd.trace.sample.rate=1 \
+        -Ddd.service=${DD_SERVICE} \
+        -Ddd.env=${DD_ENV} \
+        -Ddd.version=${DD_VERSION} \
+        -jar target/Datadog-java-0.0.1-SNAPSHOT.jar
+else
+    echo "JAR file not found: target/Datadog-java-0.0.1-SNAPSHOT.jar"
+    exit 1
+fi
